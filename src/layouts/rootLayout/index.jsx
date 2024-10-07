2beba1239ead2c9585328548ef3d0118dcee3157
Steven Gong <gonghaima@hotmail.com>
2024-10-08 08:55:32 +1100

    add clerk auth

--- src/layouts/rootLayout/index.jsx
+++ src/layouts/rootLayout/index.jsx
@@ -1,37 +1,44 @@
 import { Link, Outlet } from 'react-router-dom';
 import './rootLayout.css';
-// import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+import {
+  ClerkProvider,
+  SignedIn,
+  UserButton,
+  SignedOut,
+  SignInButton,
+} from '@clerk/clerk-react';
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-// if (!PUBLISHABLE_KEY) {
-//   throw new Error('Missing Publishable Key');
-// }
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 // const queryClient = new QueryClient();
 
 const RootLayout = () => {
   return (
-    // <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-
-    <div className="rootLayout">
-      <header>
-        <Link to="/" className="logo">
-          <img src="/logo.png" alt="" />
-          <span>LAMA AI</span>
-        </Link>
-        <div className="user">
-          {/* <SignedIn>
-            <UserButton />
-          </SignedIn> */}
-        </div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-
-    // </ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <div className="rootLayout">
+        <header>
+          <Link to="/" className="logo">
+            <img src="/logo.png" alt="" />
+            <span>LAMA AI</span>
+          </Link>
+          <div className="user">
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </ClerkProvider>
   );
 };
 
