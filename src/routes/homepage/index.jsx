80966cf412c9fa0f3916b71de1a70a78b1f04077
Steven Gong <gonghaima@hotmail.com>
2024-10-11 10:36:39 +1100

    home page ui setup

--- src/routes/homepage/index.jsx
+++ src/routes/homepage/index.jsx
@@ -1,10 +1,50 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './homepage.css';
 
 const homepage = () => {
+  const [typingStatus, setTypingStatus] = useState('human1');
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
+          dolorem doloribus, architecto dolor.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === 'human1'
+                  ? '/human1.jpeg'
+                  : typingStatus === 'human2'
+                  ? '/human2.jpeg'
+                  : 'bot.png'
+              }
+              alt=""
+            />
+            {/* <TypeAnimation/> */}
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
     </div>
   );
 };
