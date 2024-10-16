f4ea1680dfabb92eb184b68c0d2244d4f08ea79e
Steven Gong <gonghaima@hotmail.com>
2024-10-16 19:34:21 +1100

    initial setup chatList component

--- src/layouts/dashboardLayout/index.jsx
+++ src/layouts/dashboardLayout/index.jsx
@@ -2,7 +2,7 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import './dashboardLayout.css';
 import { useAuth } from '@clerk/clerk-react';
 import { useEffect } from 'react';
-// import ChatList from "../../components/chatList/ChatList";
+import ChatList from '../../components/chatList';
 
 const DashboardLayout = () => {
   const { userId, isLoaded } = useAuth();
@@ -19,7 +19,9 @@ const DashboardLayout = () => {
 
   return (
     <div className="dashboardLayout">
-      {/* <div className="menu"><ChatList/></div> */}
+      <div className="menu">
+        <ChatList />
+      </div>
       <div className="content">
         <Outlet />
       </div>
