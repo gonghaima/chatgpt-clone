c17b727f2b92e65c362cde2699b27f00e6a86152
Steven Gong <gonghaima@hotmail.com>
2024-10-17 19:52:29 +1100

    dashboard page static content & chatList update

--- src/components/chatList/index.jsx
+++ src/components/chatList/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './chatList.css';
 
 const ChatList = () => {
   return (
     <div className="chatList">
       <span className="title">DASHBOARD</span>
-      {/* <Link to="/dashboard">Create a new Chat</Link>
+      <Link to="/dashboard">Create a new Chat</Link>
       <Link to="/">Explore Lama AI</Link>
-      <Link to="/">Contact</Link> */}
+      <Link to="/">Contact</Link>
       <hr />
       <span className="title">RECENT CHATS</span>
       <div className="list">
@@ -20,6 +21,12 @@ const ChatList = () => {
                 {chat.title}
               </Link>
             ))} */}
+            <Link to="/dashboard/chats/1">Chat 1</Link>
+            <Link to="/dashboard/chats/1">Chat 1</Link>
+            <Link to="/dashboard/chats/1">Chat 1</Link>
+            <Link to="/dashboard/chats/1">Chat 1</Link>
+            <Link to="/dashboard/chats/1">Chat 1</Link>
+            <Link to="/dashboard/chats/1">Chat 1</Link>
       </div>
       <hr />
       <div className="upgrade">
