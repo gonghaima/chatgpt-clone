1a95953c14ef0d751ee3f877869ec17bd01c0813
Steven Gong <gonghaima@hotmail.com>
2024-11-02 12:24:04 +1100

    update image - server side

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-console.log('test');
+import express from 'express';
+import ImageKit from 'imagekit';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+});
+
+app.get('/api/upload', (req, res) => {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
