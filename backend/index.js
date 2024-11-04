3c7bfd26c5bd05222cdbb7a2c303e1fd3bde45ae
Steven Gong <gonghaima@hotmail.com>
2024-11-04 12:09:03 +1100

    chatList - cannot be verified, until create chat feature added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,36 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import url, { fileURLToPath } from 'url';
 import ImageKit from 'imagekit';
+import mongoose from 'mongoose';
+import Chat from './models/chat.js';
+import UserChats from './models/userChats.js';
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -15,6 +43,21 @@ app.get('/api/upload', (req, res) => {
   res.send(result);
 });
 
+app.get('/api/userchats', ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+
+  try {
+    const userChats = await UserChats.find({ userId });
+
+    res.status(200).send(userChats[0].chats);
+  } catch (err) {
+    console.log(err);
+    res.status(200).send();
+    // res.status(500).send('Error fetching userchats!');
+  }
+});
+
 app.listen(port, () => {
+  connect();
   console.log(`Server is running on port ${port}`);
 });
