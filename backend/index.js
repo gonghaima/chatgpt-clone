df36951eddba54f434611c5dadcc5ed9526db472
Steven Gong <gonghaima@hotmail.com>
2024-11-05 17:02:37 +1100

    send promt to backend, then saved to db

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,57 @@ app.get('/api/upload', (req, res) => {
   res.send(result);
 });
 
+app.post('/api/chats', ClerkExpressRequireAuth(), async (req, res) => {
+  const userId = req.auth.userId;
+  const { text } = req.body;
+
+  try {
+    // CREATE A NEW CHAT
+    const newChat = new Chat({
+      userId: userId,
+      history: [{ role: 'user', parts: [{ text }] }],
+    });
+
+    const savedChat = await newChat.save();
+
+    // CHECK IF THE USERCHATS EXISTS
+    const userChats = await UserChats.find({ userId: userId });
+
+    // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+          },
+        ],
+      });
+
+      await newUserChats.save();
+    } else {
+      // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+      await UserChats.updateOne(
+        { userId: userId },
+        {
+          $push: {
+            chats: {
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          },
+        }
+      );
+
+      res.status(201).send(newChat._id);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error creating chat!');
+  }
+});
+
 app.get('/api/userchats', ClerkExpressRequireAuth(), async (req, res) => {
   const userId = req.auth.userId;
 
